231c265d4e177ac25a3bb458bfc37145463a915f
enayzuin <42486373+enayzuin@users.noreply.github.com>
2022-12-13 21:23:49 -0300

    Update HelloWorld.java

--- src/com/jes/aulas/HelloWorld.java
+++ src/com/jes/aulas/HelloWorld.java
@@ -5,3 +5,29 @@ public class HelloWorld {
         System.out.println("Hello World");
     }
 }
+
+
+//TODO
+//Existe o seguinte cenário: Você está na sua empresa e sua internet caiu, descreva o passo a passo para acionar a area de infra
+//e resolver o seu problema (minimo 20 passos)
+
+ 
+//TODO Montar um mapa mental da aula 1
+
+//Aula 1:
+
+//Parte eletrica (Zeros e Uns):
+//Binário para Hexadecimal
+//Portas logicas (AND, NOT, OR, *XOR)
+//*Exclusiva
+
+//Programação é um facilitador
+
+//Compilação - https://www.instagram.com/p/CkRq49cuJOk/
+//Instalar JDK
+//javac arquivo.java
+//java arquivo (sem extensão)
+
+//Organização - package, main, src
+//Bibliotecas nativas - O pacote java. lang contém as classes que constituem recursos básicos da linguagem, necessários à execução de qualquer programa Java.
+
