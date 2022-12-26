68e5a3c46b46f6846acc6b7e0deee5f3ec93895f
John Enderson <johnenderson@outlook.com>
2022-12-26 11:35:57 -0300

    Problema com o Switch/Case

--- src/com/jes/aulas/NewTicket.java
+++ src/com/jes/aulas/NewTicket.java
@@ -8,23 +8,67 @@ public class NewTicket {
 
     	Scanner in = new Scanner(System.in);
         System.out.println("Seja bem vindo ao sistema de chamados, insira seu nome para iniciar:");
-        String name = in.nextLine();
-        System.out.println("Certo " + name.toUpperCase() + " vamos começar!");
+        chamado.setIniciadorChamado(in.nextLine().toUpperCase());
+        System.out.println("Certo " + chamado.getIniciadorChamado() + " vamos começar!");
+        System.out.println("Você é o solicitante do chamado? (Digite 1 para 'SIM' ou 2 para 'NÃO'");
+        int opcao = in.nextInt();
 
-        System.out.println(chamado.getTipoChamado());
-        int type = in.nextInt();
-
-        switch(type) {
+        switch (opcao) {
             case 1:
-                System.out.println("Segurança da informação, selecionado.");
+                chamado.setSolicitanteChamado(chamado.getIniciadorChamado());
+                System.out.println("Ok, " + chamado.getIniciadorChamado() + "!");
                 break;
             case 2:
-                System.out.println("Infraestrutura, selecionado.");
+                System.out.println("Digite o nome do solicitante: ");
+                String solicitante = in.nextLine();
+                chamado.setSolicitanteChamado(solicitante);
+                System.out.println(chamado.getSolicitanteChamado());
+                break;
+            default:
+                System.out.println("O número escolhido é inválido! Digite um número entre 1 e 2.");
+        }
+
+        System.out.println("Informe qual é o Titulo para este chamado:");
+        String titulo = in.nextLine();
+        chamado.setTituloChamado(titulo);
+
+        System.out.println(chamado.getTipoChamado());
+        int tipochamado = in.nextInt();
+
+        switch(tipochamado) {
+            case 1:
+                System.out.println("Segurança da informação, selecionado!");
+                break;
+            case 2:
+                System.out.println("Infraestrutura, selecionado!");
                 break;
             case 3:
-                System.out.println("Sistemas, selecionado.");
+                System.out.println("Sistemas, selecionado!");
                 break;
+            default:
+                System.out.println("O número escolhido é inválido! Digite um número entre 1 e 3.");
         }
+
+
+        //E-mails adicionais a serem notificados, separados por ";". (Ex: e-mail1@teste.com; email2@teste.com)
+        //Qual a sua dúvida ou problema? (Se for um problema, detalhe o máximo possível incluindo como anexo a tela do erro)
+
+        System.out.println("------------------------------");
+        System.out.println("INFORMAÇÕES DO CHAMADO");
+        System.out.println("------------------------------");
+        System.out.println("Nome Iniciador: " + chamado.getIniciadorChamado());
+//        System.out.println("Nome Solicitante: " + chamado.getSolicitanteChamado());
+        System.out.println("Titulo: " + chamado.getTituloChamado());
+//        System.out.println("Tipo de Chamado: " + chamado.getTipoChamado());
+//        System.out.println("Gravidade: " + chamado.getGravidadeChamado());
+//        System.out.println("Urgência: " + chamado.getUrgenciaChamado());
+//        System.out.println("Descrição breve do chamado: " + chamado.getDescricaoChamado());
+//        System.out.println("Telefone do Solicitante: " + chamado.getTelefoneContato());
+//        System.out.println("Area do Solicitante: " + chamado.getAreaIniciador());
+//        System.out.println("Tipo de Localização do Solicitante: " + chamado.getTipoLocalizacao());
+//        System.out.println("Localização do Solicitante: " + chamado.getLocalizacao());
+//        System.out.println("E-mail do Solicitante: " + chamado.getEmail());
+//        System.out.println("E-mails adicionais a serem notificados: " + chamado.getEmailsAdicionais());
 
         in.close();
     }
--- src/com/jes/aulas/Ticket.java
+++ src/com/jes/aulas/Ticket.java
@@ -3,19 +3,19 @@ package com.jes.aulas;
 public class Ticket {
 
     private String tipoChamado = "Selecione o tipo de chamado: " + "\n" + "1 - Segurança da informação" + "\n" + "2 - Infraestrutura"  + "\n" +  "3 - Sistemas";
-    private String tituloChamado = new String();
-    private String numeroChamado = new String();
-    private String iniciadorChamado = new String();
-    private String solicitanteChamado = new String();
-    private String telefoneContato = new String();
-    private String areaIniciador = new String();
+    private String tituloChamado;
+    private String numeroChamado;
+    private String iniciadorChamado;
+    private String solicitanteChamado;
+    private String telefoneContato;
+    private String areaIniciador;
     private String tipoLocalizacao = "1. Loja," +"\n" +  "2. Fabrica" + "\n" + "3. Escritorio";
-    private String localizacao = new String();
-    private String email = new String();
-    private String emailsAdicionais = new String(); //E-mails adicionais a serem notificados, separados por ";". (Ex: e-mail1@teste.com; email2@teste.com)
+    private String localizacao;
+    private String email;
+    private String emailsAdicionais; //E-mails adicionais a serem notificados, separados por ";". (Ex: e-mail1@teste.com; email2@teste.com)
     private String gravidadeChamado = "0. Erro que impede a continuidade do projeto de implantação," + "\n" + "1. Sistema Indisponivel," + "\n" + " 2. Erro critico para operação do sistema," + "\n" + "3. Erro médio ou de menor impacto," + "\n" + " 4. Dúvidas e ou melhorias";
     private String urgenciaChamado = "1. Necessario ação imediata (Usar apenas na gravidade 1), 2. Atender o mais cedo possivel, 3. Prazo padrão de atendimento";
-    private String descricaoChamado = new String(); //Qual a sua dúvida ou problema? (Se for um problema, detalhe o máximo possível incluindo como anexo a tela do erro)
+    private String descricaoChamado; //Qual a sua dúvida ou problema? (Se for um problema, detalhe o máximo possível incluindo como anexo a tela do erro)
 
     public String getTipoChamado() {
         return tipoChamado;
